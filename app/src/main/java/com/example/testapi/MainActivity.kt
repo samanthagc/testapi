764ab667418c57958a5750770b34b51267fb4543
samanthacosta <samanthacosta@brq.com>
2019-10-08 10:42:30 -0300

    Ajuste chamada API para coroutine

--- app/src/main/java/com/example/testapi/MainActivity.kt
+++ app/src/main/java/com/example/testapi/MainActivity.kt
@@ -8,41 +8,56 @@ import com.example.testapi.adapter.MainAdapter
 import com.example.testapi.model.ResponseAPI
 import com.example.testapi.retrofit.RetrofitInitializer
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+
+    private lateinit var job : Job
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        job = Job()
         setContentView(R.layout.activity_main)
 
 
-        val call = RetrofitInitializer().responseService().list()
-        call.enqueue(object: Callback<List<ResponseAPI>?> {
-            override fun onFailure(call: Call<List<ResponseAPI>?>, t: Throwable) {
-                Log.e("onFailureError Call API", t.message)
-            }
+        GlobalScope.launch {
 
-            override fun onResponse(call: Call<List<ResponseAPI>?>, response: Response<List<ResponseAPI>?>) {
-                response.body()?.let {
-                    val responses : List<ResponseAPI> = it
-                    configureList(responses)
-                }
-            }
+            getList()?.let { configureList(it) }
 
-        })
+        }
 
     }
 
-    private fun configureList(responses : List<ResponseAPI>) {
-        val recyclerView = list_return
-        recyclerView.adapter = MainAdapter(responses, this)
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
+    }
 
-        val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
-        recyclerView.layoutManager = layoutManager
+    private fun configureList(responses : List<ResponseAPI>) {
+
+        this@MainActivity.runOnUiThread {
+            val recyclerView = list_return
+            recyclerView.adapter = MainAdapter(responses, this)
+
+            val layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
+            recyclerView.layoutManager = layoutManager
+        }
+
+    }
+
+    suspend fun getList() : List<ResponseAPI>?{
+        return try {
+            RetrofitInitializer().responseService().list()
+        }catch (e : Exception) {
+            Log.i("Erro", e.message)
+            null
+        }
+
     }
 
 
--- app/src/main/java/com/example/testapi/retrofit/service/ResponseService.kt
+++ app/src/main/java/com/example/testapi/retrofit/service/ResponseService.kt
@@ -1,12 +1,13 @@
 package com.example.testapi.retrofit.service
 
 import com.example.testapi.model.ResponseAPI
+import kotlinx.coroutines.Deferred
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface ResponseService{
 
     @GET("posts")
-    fun list() : Call<List<ResponseAPI>>
+    suspend fun list() : List<ResponseAPI>
 
 }
